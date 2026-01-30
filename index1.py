4eb62579bc524066526075ca0dcbd39ad39d50f9
Ajay07kum <ajayprajapati9810king@gmail.com>
2026-01-30 11:48:25 +0530

    index.py

---
+++ index1.py
@@ -0,0 +1,11 @@
+# print("enetr any three no")
+# a=eval(input())
+# b=eval(input())
+# c=eval(input())
+# if a>b:
+#     if a<c:
+#         print("b")
+#     else:
+#         print("c")
+# else:
+#     print ("a")        
